3730c6efca1b021476f8cd152db1414e0cabf070
sarafinifaustini <sarinethaindu@gmail.com>
2020-05-20 15:17:06 +0300

    admin login
    Admin side with admin login and a bit of the posts done

---
+++ admin/resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome to MOH ADMIN!</h1>
+        <p>MOH ADMIN</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    </div>
+@endsection
