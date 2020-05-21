0b43f4133df63167304c2969b2125df5773b2c78
amidayiera <amidayiera@gmail.com>
2020-05-21 09:09:38 +0300

    add images to the cards

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -7,49 +7,67 @@
    
         <div class="card-deck">
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+              <img class="card-img-top" src="{{ asset('images/landingPage_news3.jpg') }}" alt="News Icon">          
               <div class="card-body">
                 <h5 class="card-title">News</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
+                <p class="card-text">Condimentum mattis pellentesque id nibh. B
+                  ibendum enim facilisis gravida neque convallis a cras semper auctor. 
+                  Dictumst vestibulum rhoncus.</p>
+
             </div>
             </div>
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+              <img class="card-img-top" src="{{ asset('images/landingPage_guidelines3.png') }}"  alt="Card image cap">          
               <div class="card-body">
                 <h5 class="card-title">Health care guidelines</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Condimentum mattis pellentesque id nibh. 
+                  Bibendum enim facilisis gravida neque convallis a cras semper auctor.
+                   Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. 
+                  Ut faucibus pulvinar elementum integer enim neque</p>
              </div>
             </div>
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+ <img class="card-img-top" src="{{ asset('images/landingPage_location.png') }}"  alt="Card image cap">          
               <div class="card-body">
                 <h5 class="card-title">Health care locations</h5>
-                <p class="card-text">This is a wider card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This card has even longer content than the first to show that equal height action.</p>
-              </div>
+                <p class="card-text">Lorem ipsum dolor sit amet, 
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                  labore et dolore magna aliqua. Enim lobortis scelerisque fermentum dui faucibus in. 
+                  Et netus et malesuada fames ac turpis egestas integer.</p>
             </div>
           </div>
 
           
           <div class="card-deck">
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+ <img class="card-img-top" src="{{ asset('images/landingPage_events.png') }}"  alt="Card image cap">          
               <div class="card-body">
                 <h5 class="card-title">Events</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Enim lobortis scelerisque fermentum dui faucibus in.
+                   Et netus et malesuada fames ac turpis egestas integer.</p>
             </div>
             </div>
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+ <img class="card-img-top" src="{{ asset('images/landingPage_aboutUs.png') }}"  alt="Card image cap">          
               <div class="card-body">
                 <h5 class="card-title">About us</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Amet mauris commodo quis imperdiet massa. 
+                  Egestas diam in arcu cursus. Mi quis hendrerit dolor magna. 
+                  Lorem ipsum dolor sit amet. Tortor at auctor urna nunc id cursus. 
+                  Est pellentesque elit ullamcorper dignissim cras tincidunt.
+
+                </p>
              </div>
             </div>
             <div class="card">
- <img class="card-img-top" src="..." alt="Card image cap">          
+ <img class="card-img-top" src="{{ asset('images/landingPage_feedback3.png') }}" alt="Card image cap">          
               <div class="card-body">
                 <h5 class="card-title">Feedback</h5>
-                <p class="card-text">This is a wider card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text">Gravida neque convallis a cras semper auctor. 
+                   Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim 
+                   cras tincidunt. Ut faucibus pulvinar elementum integer enim neque..</p>
               </div>
             </div>
           </div>
